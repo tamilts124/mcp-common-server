a2a400120d9828acf21a48b126a0be07ef345f96
Tamil112233 <tamilarasan.dhanamtcs@gmail.com>
2026-07-12 16:58:21 +0100

    chore: remove scratch/fix scripts from previous session

--- fix_pkg.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const fs = require('fs');
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-pkg.version = '4.172.0';
-pkg.scripts['test:kafka-client'] = 'node test/sections/200-kafka-client.js';
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
-console.log('Done:', pkg.version, pkg.scripts['test:kafka-client']);
