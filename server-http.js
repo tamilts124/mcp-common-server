d44279d5aae1c95e4fe7d75bcb34c4af5408674b
Tamil112233 <tamilarasan.dhanamtcs@gmail.com>
2026-07-26 13:39:55 +0100

    Fix syntax error in server-http.js

--- server-http.js
+++ server-http.js
@@ -94,9 +94,9 @@ const CATEGORY_TOOLS = {
     "write_file","write_files","create_file","create_files","replace_in_file",
     "truncate_file","append_file","base64_decode","delete_file","delete_files",
     "move_file","copy_file","move_directory","copy_directory","create_directory",
-    \"screen_capture\",\"run_command\",\"start_process\",\"get_process_output\",\"kill_process\",
-    \"list_processes\",\"run_npm_script\",\"execute_pipeline\",\"send_process_input\",
-    \"screen_capture\",
+    "screen_capture","run_command","start_process","get_process_output","kill_process",
+    "list_processes","run_npm_script","execute_pipeline","send_process_input",
+    "screen_capture",
 
     "pdf_to_docx","json_path_set",
   ]),
